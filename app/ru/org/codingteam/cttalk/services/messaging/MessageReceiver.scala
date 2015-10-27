26b9f7d65053d4801d035c7f0ada2af3c8f2f047
Hagane <deemson@gmail.com>
2015-10-28 01:31:38 +0600

    messaging backend works. probably.

--- app/ru/org/codingteam/cttalk/services/messaging/MessageReceiver.scala
+++ app/ru/org/codingteam/cttalk/services/messaging/MessageReceiver.scala
@@ -2,9 +2,13 @@ package ru.org.codingteam.cttalk.services.messaging
 
 import ru.org.codingteam.cttalk.models.Message
 
+import scala.concurrent.{Future, Promise}
+
 /**
  * Created by hgn on 25.10.2015.
  */
 trait MessageReceiver {
-  def receive(message: Message): Boolean
+  def receive(message: Message): Future[Boolean]
+
+  def get(): Promise[Seq[Message]]
 }
--- app/ru/org/codingteam/cttalk/services/messaging/SingleUserMessageReceiver.scala
+++ app/ru/org/codingteam/cttalk/services/messaging/SingleUserMessageReceiver.scala
@@ -1,15 +1,27 @@
 package ru.org.codingteam.cttalk.services.messaging
 
-import ru.org.codingteam.cttalk.models.Message
+import ru.org.codingteam.cttalk.models.{Message, Token}
 import ru.org.codingteam.cttalk.services.MessagesRepository
 
-import scala.concurrent.Future
+import scala.concurrent.{Future, Promise}
+import scala.util.{Failure, Success}
 
 /**
  * Created by hgn on 27.10.2015.
  */
-class SingleUserMessageReceiver(messagesRepository: MessagesRepository) extends MessageReceiver {
+class SingleUserMessageReceiver(token: Token, messagesRepository: MessagesRepository) extends MessageReceiver {
+
   override def receive(message: Message): Future[Boolean] = {
     messagesRepository.save(message) map { _ => true } recover { case _ => false }
+  }
+
+  override def get(): Promise[Seq[Message]] = {
+    val messagesPromise = Promise[Seq[Message]]()
+    messagesRepository.getUnreadFor(token) onComplete {
+      case Success(Seq()) => messagesPromise.failure(new RuntimeException) //todo wait for receive call and attempt recovery here
+      case Success(seq) => messagesPromise.success(seq)
+      case Failure(throwable) => messagesPromise.failure(throwable)
+    }
+    messagesPromise
   }
 }
