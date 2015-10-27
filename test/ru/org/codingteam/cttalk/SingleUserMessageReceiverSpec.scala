10bbea17b19b51dc7162867e951c2ac9d6b1202a
Hagane <deemson@gmail.com>
2015-10-28 01:42:08 +0600

    single user message receiver spec

---
+++ test/ru/org/codingteam/cttalk/SingleUserMessageReceiverSpec.scala
@@ -0,0 +1,32 @@
+package ru.org.codingteam.cttalk
+
+import play.api.test.PlaySpecification
+
+/**
+ * Created by hgn on 28.10.2015.
+ */
+class SingleUserMessageReceiverSpec extends PlaySpecification {
+  "SingleUserMessageReceiver.receive" should {
+    "-- relay receved message to repository" in {
+      todo
+    }
+
+    "-- trigger fulfillment of a promise from .get" in {
+      todo
+    }
+  }
+
+  "SingleUserMessageReceiver.get" should {
+    "-- return an eventually successfull promise if there are unread messages" in {
+      todo
+    }
+
+    "-- return a promise that will be successfull after receiving a message" in {
+      todo
+    }
+
+    "-- return a promise that will be failed if call to repository was failed" in {
+      todo
+    }
+  }
+}
